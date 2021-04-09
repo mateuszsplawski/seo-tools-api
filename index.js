4fcde0ba3c8c9eaea6135ea3e472c0a1ba086fac
Mateusz <mateusz.splawski96@gmail.com>
2021-04-09 22:52:17 +0200

    refactor(dist): Restructure files for netlify deploy

--- index.js
+++ index.js
@@ -10,10 +10,22 @@ app.get("/meta/:url", (req, res) => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
-    await page.goto(`http://${req.params.url}`);
+    await page.goto(req.params.url, {
+      waitUntil: "networkidle0",
+    });
 
     const title = await page.title();
-    res.send(title);
+    const description = await page.evaluate(
+      () => document.querySelector("head > meta[name='description']").content
+    );
+    const keywords = await page.evaluate(() => {
+      if (document.querySelector("head > meta[name='keywords']")) {
+        return document.querySelector("head > meta[name='keywords']").content;
+      }
+    });
+
+    res.send({ title, description, keywords });
+    console.log("End");
     await browser.close();
   })();
 });
