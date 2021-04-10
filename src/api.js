ec87c98245e6c2326fdbe6222e1c7472fd3413a7
Mateusz <mateusz.splawski96@gmail.com>
2021-04-10 14:02:13 +0200

    feat(api): Add cors middleware

--- src/api.js
+++ src/api.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const serverless = require("serverless-http");
+const cors = require("cors");
 
 const router = require("./router");
 const app = express();
 
 app.use("/.netlify/functions/api", router);
+app.use(cors());
 
 module.exports.handler = serverless(app);
